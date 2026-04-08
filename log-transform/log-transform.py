2de9a3d5ce2c8a32754d89e7dcba48ad940d5b52
Henry Ngo <120741841+gitHuyNgo@users.noreply.github.com>
2026-04-09 00:56:14 +0700

    Add solution for log-transform

---
+++ log-transform/log-transform.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def log_transform(values):
+    """
+    Apply the log1p transformation to each value.
+    """
+    # Write code here
+    values = np.asarray(values)
+    
+    return np.log(1 + values)
